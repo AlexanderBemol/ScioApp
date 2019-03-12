31423ecacf318529fe55abfa75f5f89fd3ebdea9
Alexander <aleex1742@gmail.com>
2019-03-11 17:57:44 -0700

    creados controlador y modelo con metodos necesarios para registrar un usuario

---
+++ app/src/main/java/com/nordokod/scio/Controller/SignUpController.java
@@ -0,0 +1,40 @@
+package com.nordokod.scio.Controller;
+
+import android.app.Activity;
+import android.content.Context;
+import com.nordokod.scio.Entidad.User;
+import com.nordokod.scio.Model.SignUpModel;
+
+public class SignUpController {
+    SignUpModel signModel;
+    //SignUpActivity signActivity;
+    User user;
+    Activity currentActivity;
+    Context currentContext;
+    public SignUpController(Context cCon,Activity cAct){
+        signModel=new SignUpModel(this,cAct,cCon);
+        user=new User();
+        currentActivity=cAct;
+        currentContext=cCon;
+    }
+    public void signUpUser(String email,String password1,String password2){
+        if(password1.length()>=8){
+            if(password1.equals(password2)){
+                user.setEmail(email);
+                user.setPassword(password1);
+                signModel.signUpUser(user);
+            }else{
+                //no son iguales las contraseñas
+            }
+        }else{
+            //contraseña muy corta
+        }
+    }
+
+    public void correctSignUp(){
+
+    }
+    public void incorrectSignUp(String error){
+
+    }
+}
---
+++ app/src/main/java/com/nordokod/scio/Model/SignUpModel.java
@@ -0,0 +1,61 @@
+package com.nordokod.scio.Model;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.nordokod.scio.Controller.SignUpController;
+import com.nordokod.scio.Entidad.User;
+
+import java.util.Objects;
+
+public class SignUpModel {
+    private FirebaseAuth mAuth;
+    SignUpController SignController;
+    Activity currentActivity;
+    Context currentContext;
+
+    public SignUpModel(SignUpController SignC,Activity cAct,Context cCon){
+        this.SignController=SignC;
+        mAuth = FirebaseAuth.getInstance();
+        currentActivity=cAct;
+        currentContext=cCon;
+    }
+    public void signUpUser(User user){
+        mAuth.createUserWithEmailAndPassword(user.getEmail(),user.getPassword())
+                .addOnCompleteListener(currentActivity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {//se registró correctamente
+                            sendVerificationEmail();//enviar correo verificación
+                            SignController.correctSignUp();//avisar controller
+                        } else {
+                            if(task.getException()!=null) {//evitar nullPointerException
+                                SignController.incorrectSignUp(task.getException().toString());//avisar controller con error
+                            }
+                        }
+                    }
+                });
+    }
+    private void sendVerificationEmail()
+    {
+        FirebaseUser fbsUser = mAuth.getCurrentUser();
+        if (fbsUser != null) {
+            fbsUser.sendEmailVerification()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                             //se envió
+                            }
+                        }
+                    });
+        }
+    }
+
+}
